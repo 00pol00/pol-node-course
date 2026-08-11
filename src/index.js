b0909efe7f7c6f19d8708a0c3c8281f5dbf532e3
paul <hpribera@gmail.com>
2026-08-11 01:44:18 -0400

    segundo commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 const app= express()
 
 
-app.listen(3000)
-console.log('Server is running on port 3000')
+app.listen(process.env.PORT || 3000)
+console.log('Server is running on port',process.env.PORT || 3000)   
 
 
 app.get('/', (req, res) => {
